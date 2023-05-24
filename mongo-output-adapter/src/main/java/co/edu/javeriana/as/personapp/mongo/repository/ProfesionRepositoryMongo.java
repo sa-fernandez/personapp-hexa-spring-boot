700135925ab5519f02c5fa9de616a32d73184495
andresito29 <70115938+AndresVR14@users.noreply.github.com>
2023-05-23 20:29:00 -0500

    Mongo adapters added

--- mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/repository/ProfesionRepositoryMongo.java
+++ mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/repository/ProfesionRepositoryMongo.java
@@ -3,9 +3,9 @@ package co.edu.javeriana.as.personapp.mongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import co.edu.javeriana.as.personapp.mongo.document.PersonaDocument;
+import co.edu.javeriana.as.personapp.mongo.document.ProfesionDocument;
 
 @Repository
-public interface PersonaRepositoryMongo extends MongoRepository<PersonaDocument, Integer> {
+public interface ProfesionRepositoryMongo extends MongoRepository<ProfesionDocument, Integer> {
 
 }
--- mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/repository/TelefonoRepositoryMongo.java
+++ mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/repository/TelefonoRepositoryMongo.java
@@ -3,9 +3,9 @@ package co.edu.javeriana.as.personapp.mongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import co.edu.javeriana.as.personapp.mongo.document.PersonaDocument;
+import co.edu.javeriana.as.personapp.mongo.document.TelefonoDocument;
 
 @Repository
-public interface PersonaRepositoryMongo extends MongoRepository<PersonaDocument, Integer> {
+public interface TelefonoRepositoryMongo extends MongoRepository<TelefonoDocument, String> {
 
 }
